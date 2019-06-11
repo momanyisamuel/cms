c9e60f356c540c7c64f5ade8972aed99f21e3679
mburuwakimani <k2mburu@gmail.com>
2019-06-11 14:15:19 +0300

    no changes

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.configure(function(){
     app.set('views', path.join(__dirname, '/client/views'));
     app.set('view engine', 'ejs');
     app.use(express.logger('dev'));
-    app.use(express.json());``
+    app.use(express.json());
     app.use(express.urlencoded());
     app.use(express.methodOverride());
     app.use(express.multipart());
